6ee7ee6e69535b2eb6c7b048fbc5d47083d856aa
zyando <zyando.iron79@gmail.com>
2018-08-22 03:10:40 +0900

    Update

--- HyperbolicPatterns.py
+++ HyperbolicPatterns.py
@@ -9,7 +9,7 @@ clr.AddReference("VecMath")
 from VecMath import *
 import math
 
-MAX_LAYER = 8
+MAX_LAYER = 3
 EPS = 1E-5
 
 triangles = []
@@ -52,32 +52,59 @@ def invert(center, r, p):
 
     return center + +v * (r ** 2 / d)
 
-def add_triangle(p, l1, l2, layer):
-    triangles.append((p, l1, l2))
+def add_triangle(vertices, UNIT_ANGLE = math.pi / 36):
+    center = (vertices[0] + vertices[1] + vertices[2]) * (1.0 / 3)
 
-    if layer >= MAX_LAYER: return
-    
-    if l1.Length() < EPS or l2.Length() < EPS:
-        p_inv = reflect(+(l1 - l2), p)
-    else:
-        l3 = invert(Vector2.Zero, 1, l1)
-        successed, center, r = circle_center_pos(l1, l2, l3)
-
-        if not successed:
-            print "Did not Successed! layer:" + str(layer)
+    def add_edge(e1, e2):
+        if e1.Length() < EPS or e2.Length() < EPS or (+e1 - +e2).Length() < EPS:
+            triangles.append((center, e1, e2))
             return
-        p_inv = invert(center, r, p)
+        
+        successed, c, r = circle_center_pos(e1, e2, invert(Vector2.Zero, 1, e1))
+        
+        v1 = +(e1 - c)
+        v2 = +(e2 - c)
+
+        angle = math.acos(v1 * v2)
+        num_split = int(round(abs(angle) / UNIT_ANGLE))
+
+        if num_split == 0:
+            triangles.append((center, e1, e2))
+            return
+
+        mult = 1.0 / num_split
+
+        for i in range(num_split):
+            triangles.append((center, c + Vector2.Interpolate(v2, v1, mult * i) * r, c + Vector2.Interpolate(v2, v1, mult * (i + 1)) * r))
+
+    for i in range(3):
+        add_edge(vertices[i], vertices[(i + 1) % 3])
+
+def add_motif(vertices, center, layer):
+    def getvtx(idx, num = len(vertices)):
+        return vertices[idx % num]
+
+    for i in range(len(vertices)):
+        add_triangle((center, getvtx(i), getvtx(i + 1)))
     
-    if layer > 1: add_triangle(l1, l2, p_inv, layer + 1)
-    add_triangle(l2, p_inv, l1, layer + 1)
+    if layer >= MAX_LAYER:
+        return
 
-v1 = Vector2(0.26012, -0.450529)
-m = Matrix2.Rotation(math.pi / 3)
+    for i in range(len(vertices)):
+        edge = [getvtx(i), getvtx(i + 1)]
+        successed, c, r = circle_center_pos(edge[0], edge[1], invert(Vector2.Zero, 1, edge[0]))
 
-for i in range(6):
-    v2 = v1 * m
-    add_triangle(Vector2.Zero, v1, v2, 0)
-    v1 = v2
+        invert_center = invert(c, r, center)
+
+        if invert_center.Length() < center.Length():
+            continue
+
+        inverted_vertices = edge + [invert(c, r, getvtx(i + 2 + j)) for j in range(4)]
+
+        add_motif(inverted_vertices, invert_center, layer + 1)
+
+v = Vector2(0, (math.sqrt(6) - math.sqrt(2)) * 0.5)
+add_motif([v * Matrix2.Rotation(math.pi / 3 * i) for i in range(6)], Vector2.Zero, 0)
 
 with open("Hyperbolic.obj", "w") as f:
     vertices_lines = []
@@ -91,10 +118,7 @@ with open("Hyperbolic.obj", "w") as f:
             line = "v " + " ".join([str(vtx[i]) for i in range(2)]) + " 0\n"
             vertices_lines.append(line)
 
-        line = "f "
-        for vtx in tri:
-            line += str(1 + next(iter([j for j in range(num_vertices) if Vector2.EpsilonEquals(vertices[j], vtx, EPS)]), False)) + "// "
-        triangles_lines.append(line + "\n")
+        line = "f " + " ".join([str(idx * 3 + j + 1) + "//" for j in range(3)]) + "\n"
+        triangles_lines.append(line)
     
     f.writelines(["o Hyperbolic\n"] + vertices_lines + triangles_lines)
-
